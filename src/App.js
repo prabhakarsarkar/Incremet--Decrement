1a3757548823593965ed77b5dd0f09d2a488da7d
RAJ 143 <52394727+prabhakarsarkar@users.noreply.github.com>
2020-10-25 12:11:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Increment from './component/SetStatea'
+
+function App() {
+  return (
+    <div className="App">
+  <Increment/>
+   
+    </div>
+  );
+}
+
+export default App;
